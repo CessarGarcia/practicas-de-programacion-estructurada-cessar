74b4b23edb9527a8e0ac0a80cc08ea4601d76622
CessarGarcia <64294314+CessarGarcia@users.noreply.github.com>
2020-10-14 19:03:33 -0500

    Delete 1. Hola mundo.cpp

--- 1. Hola mundo.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>				//Es una biblioteca basica del lenguaje c
-
-int main (){					//Esta es la cabecera del programa
-	
-	printf("Hola mundo!!!");	// La funcion printf permite escribir en pantalla lo que esta con "_" siempre terminar con ;
-	return 0;					// es una buena practica de programacion cerar con return 
-}								// La llave marca lo que hara ese espacio de codigo
